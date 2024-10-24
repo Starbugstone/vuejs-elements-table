5b6b983b7a23d3f53a142ee95a50fef9af9efa9d
Starbugstone <pint2stone@gmail.com>
2024-10-24 15:48:31 +0200

    test

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -166,7 +166,7 @@ onMounted(() => {
         v-model:page-size="pageSize"
         :total="totalItems"
         :page-sizes="[10, 20, 50, 100]"
-        layout="total, sizes, prev, pager, next, jumper"
+        layout="total, sizes, prev, pager, next, , jumper"
         background
       />
     </div>
